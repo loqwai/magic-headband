900f77df95861feb16110dc8ebd5e89ff5779f64
redaphid <iam@aaronherres.com>
2015-02-18 23:20:23 -0700

    it connects!!

--- app/src/main/java/com/loqwai/magic_headband/BlinkyTape.java
+++ app/src/main/java/com/loqwai/magic_headband/BlinkyTape.java
@@ -1,13 +1,18 @@
 package com.loqwai.magic_headband;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
 
+import com.hoho.android.usbserial.driver.CdcAcmSerialDriver;
 import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
 import com.hoho.android.usbserial.driver.UsbSerialProber;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
@@ -17,15 +22,29 @@ import java.util.List;
 public class BlinkyTape {
     private UsbManager manager;
     private UsbDevice device;
+    private UsbDeviceConnection connection;
+    private UsbSerialDriver driver;
+    private UsbSerialPort port;
 
     public BlinkyTape(UsbManager manager, UsbDevice device) {
         this.manager = manager;
         this.device = device;
+        this.driver = new CdcAcmSerialDriver(device);
+        port = driver.getPorts().get(0);
     }
 
-    public void connect () {
-        UsbDeviceConnection connection = manager.openDevice(device);
-        System.out.println(connection.getSerial());
+    public boolean connect() throws IOException {
+        connection = manager.openDevice(device);
+        if(connection == null) {
+            return false;
+        }
+        port.open(connection);
+        port.setParameters(57600, 8, 1, 0);
+        byte[] bytes = new byte[64 * 3];
+        for( int i = 0; i < bytes.length; i++)
+            bytes[i] = (byte)(Math.random() * 255);
+        port.write(bytes, 500);
+        return true;
     }
 
     public static BlinkyTape findBlinkyTape(UsbManager manager) {
@@ -36,7 +55,10 @@ public class BlinkyTape {
             }
         }
 
-
         return null;
+    }
+
+    public UsbDevice getDevice() {
+        return device;
     }
 }
--- app/src/main/java/com/loqwai/magic_headband/MainActivity.java
+++ app/src/main/java/com/loqwai/magic_headband/MainActivity.java
@@ -1,28 +1,84 @@
 package com.loqwai.magic_headband;
 
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+    private static final String TAG = "MagicHeadbandMain";
+    private static final String ACTION_USB_PERMISSION = "USB_PERMISSION_GRANTED";
+
+    private BroadcastReceiver receiver;
+    private BlinkyTape blinkyTape;
+    private UsbManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Find all available drivers from attached devices.
-        BlinkyTape blinkyTape =
-                BlinkyTape.findBlinkyTape((UsbManager) getSystemService(Context.USB_SERVICE));
+        manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent.getAction().equals(ACTION_USB_PERMISSION)) {
+                    Log.d(TAG, "got permission to use the blinky!");
+                    if(blinkyTape == null) {
+                        blinkyTape = BlinkyTape.findBlinkyTape(manager);
+                    }
 
-        blinkyTape.connect();
+                    if(blinkyTape == null) {
+                        Log.d(TAG, "got permission to use the blinky, but can't find it.");
+                    }
+
+                    try {
+                        blinkyTape.connect();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (intent.getAction().equals("android.hardware.usb.action.USB_DEVICE_ATTACHED")) {
+                    Log.d(TAG, "I saw a USB device attach.");
+                    blinkyTape = BlinkyTape.findBlinkyTape(manager);
+                    if(blinkyTape == null) {
+                        return;
+                    }
+
+                    try {
+                        if (!blinkyTape.connect()) {
+                            askForPermission();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        };
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_USB_PERMISSION);
+        filter.addAction("android.hardware.usb.action.USB_DEVICE_ATTACHED");
+        registerReceiver(receiver, filter);
         setContentView(R.layout.activity_main);
+    }
+
+    private void askForPermission() {
+        Log.d(TAG, "asking for permission");
+        manager.requestPermission(blinkyTape.getDevice(),
+                PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0));
     }
 
 
